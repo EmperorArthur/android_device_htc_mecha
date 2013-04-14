150f9a757cb43cf61c56a57bc442774bfade3921
Joe Black <jblack248@gmail.com>
2013-04-14 14:30:15 -0400

    Revise device build system

--- cm.mk
+++ cm.mk
@@ -1,29 +1,22 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit device configuration
+$(call inherit-product, device/htc/mecha/full_mecha.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := mecha
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/htc/mecha/device_mecha.mk)
-
-# Device naming
-PRODUCT_DEVICE := mecha
-PRODUCT_NAME := cm_mecha
-PRODUCT_BRAND := verizon_wwe
-PRODUCT_MODEL := HTC Thunderbolt
-PRODUCT_MANUFACTURER := HTC
-PRODUCT_PROPERTY_OVERRIDES += ro.modversion=cm.mecha.$(shell date +%m%d%y).$(shell date +%H%M%S)
-
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=htc_mecha \
+    PRODUCT_NAME=mecha \
+    TARGET_DEVICE=mecha \
+    BUILD_FINGERPRINT="verizon-wwe/mecha/mecha:4.0.4/IMM76L/370649.2:user/release-keys" \
+    PRIVATE_BUILD_DESC="7.00.605.2 CL370649 release-keys" \
     BUILD_ID=IMM76L \
     BUILD_NUMBER=370649.2 \
     TARGET_BUILD_TYPE=user \
-    BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="7.00.605.2 CL370649 release-keys" \
-    BUILD_FINGERPRINT=verizon_wwe/htc_mecha/mecha:4.0.4/IMM76L/370649.2:user/release-keys
+    BUILD_VERSION_TAGS=release-keys
+    
+PRODUCT_NAME := cm_mecha
+PRODUCT_DEVICE := mecha
