838ff5f094fc91556d09973c603e2094ca545947
Joe Black <jblack248@gmail.com>
2013-04-18 00:00:15 -0400

    Update vendor proprietary file list

--- extract-files.sh
+++ extract-files.sh
@@ -23,9 +23,9 @@ mkdir -p $BASE
 for FILE in `egrep -v '(^#|^$|^ )' proprietary-blobs.txt`; do
 DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
-mkdir -p $BASE/$DIR
+        mkdir -p $BASE/$DIR
     fi
-adb pull /system/$FILE $BASE/$FILE
+    adb pull /system/$FILE $BASE/$FILE
 done
 
 ./setup-makefiles.sh
