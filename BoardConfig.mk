d13f14d56f79e14693486b1da49c0d8909e1196e
liquid0624 <rjbirnesser@gmail.com>
2013-07-12 15:26:11 -0400

    Import liquids boardconfig & device config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,6 @@
-# Copyright (C) 2009 The Android Open Source Project
+#
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The LiquidSmooth Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,59 +13,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
 #
 
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
-
-# inherit from common msm7x30
+# common
 -include device/htc/msm7x30-common/BoardConfigCommon.mk
 
-# inherit from the proprietary version
+# include
 -include vendor/htc/mecha/BoardConfigVendor.mk
 
+# board
 TARGET_BOOTLOADER_BOARD_NAME := mecha
 
-# camera fixes
+# camera
 BOARD_HAVE_HTC_FFC := true
 
-# Use stock libril for now
-TARGET_PROVIDES_LIBRIL := vendor/htc/mecha/proprietary/libril.so
-BOARD_MOBILEDATA_INTERFACE_NAME := "rmnet_sdio0"
-BOARD_HAS_EXTRA_SYS_PROPS := true
-USE_IPV6_ROUTE := true
+# ril
+BOARD_RIL_CLASS := "../../../device/htc/mecha/prebuilt/ril/"
 
-# GPS
+# gps
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := mecha
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
-# Filesystem
-TARGET_USERIMAGES_USE_EXT4 := true
+# images
+BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
-BOARD_FLASH_BLOCK_SIZE := 262144
 
-# Kernel
+# kernel
+BOARD_PAGE_SIZE := 4096
+BOARD_KERNEL_BASE := 0x05200000
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
 TARGET_KERNEL_SOURCE := kernel/htc/msm7x30-3.0
 TARGET_KERNEL_CONFIG := evervolv_mecha_defconfig
-BOARD_KERNEL_CMDLINE := no_console_suspend=1
-BOARD_KERNEL_BASE := 0x05200000
-BOARD_PAGE_SIZE := 4096
 
-# sdcard blocks
+# sdcard
+BOARD_USES_MMCUTILS := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
 BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
-BOARD_USES_MMCUTILS := true
--- device_mecha.mk
+++ device_mecha.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2011 The CyanogenMod Project
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The LiquidSmooth Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,93 +15,106 @@
 # limitations under the License.
 #
 
-# common msm7x30 configs
-$(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
-
-$(call inherit-product-if-exists, vendor/htc/mecha/mecha-vendor.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# htc audio settings
-$(call inherit-product, device/htc/mecha/media_htcaudio.mk)
-$(call inherit-product, device/htc/mecha/media_a1026.mk)
-
+# overlay
 DEVICE_PACKAGE_OVERLAYS += device/htc/mecha/overlay
 
+# common
+$(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
+
+# vendor
+$(call inherit-product-if-exists, vendor/htc/mecha/mecha-vendor.mk)
+
+# telephony
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 800
+
+# boot
 PRODUCT_COPY_FILES += \
     device/htc/mecha/ramdisk/fstab.mecha:root/fstab.mecha \
     device/htc/mecha/ramdisk/init.mecha.rc:root/init.mecha.rc \
-    device/htc/mecha/ramdisk/init.mecha.usb.rc:root/init.mecha.usb.rc \
-    device/htc/mecha/ramdisk/ueventd.mecha.rc:root/ueventd.mecha.rc
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase.yt=android-htc \
-    ro.com.google.clientidbase.am=android-verizon \
-    ro.com.google.clientidbase.vs=android-verizon \
-    ro.com.google.clientidbase.gmm=android-verizon \
-    ro.com.google.clientidbase.ms=android-verizon \
-    ro.com.google.locationfeatures=1 \
-    ro.com.google.networklocation=1 \
-    ro.setupwizard.enable_bypass=1 \
-    dalvik.vm.lockprof.threshold=500 \
-    dalvik.vm.dexopt-flags=m=y \
-    ro.telephony.call_ring.delay=500 \
-    ro.telephony.call_ring.multiple=false \
-    persist.telephony.support_ipv6=true \
-    persist.telephony.support_ipv4=true
-
-# Devie XML Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    device/htc/mecha/ramdisk/ueventd.mecha.rc:root/ueventd.mecha.rc \
+    device/htc/mecha/ramdisk/init.mecha.usb.rc:root/init.mecha.usb.rc
 
 # configs
 PRODUCT_COPY_FILES += \
     device/htc/mecha/configs/gps.conf:system/etc/gps.conf \
-    device/htc/mecha/configs/voicemail-conf.xml:system/etc/voicemail-conf.xml \
-    device/htc/mecha/configs/vold.fstab:system/etc/vold.fstab
+    device/htc/mecha/configs/vold.fstab:system/etc/vold.fstab \
+    device/htc/mecha/configs/apns-conf.xml:system/etc/apns-conf.xml \
+    device/htc/mecha/configs/voicemail-conf.xml:system/etc/voicemail-conf.xml
 
+# dsp
+PRODUCT_COPY_FILES += \
+    device/htc/mecha/dsp/HP_Audio.csv:system/etc/HP_Audio.csv \
+    device/htc/mecha/dsp/HP_Video.csv:system/etc/HP_Video.csv \
+    device/htc/mecha/dsp/AudioBTID.csv:system/etc/AudioBTID.csv \
+    device/htc/mecha/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+    device/htc/mecha/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    device/htc/mecha/dsp/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
+    device/htc/mecha/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
+    device/htc/mecha/dsp/CodecDSPID_BCLK.txt:system/etc/CodecDSPID_BCLK.txt \
+    device/htc/mecha/dsp/SPK_Combination.csv:system/etc/SPK_Combination.csv \
+    device/htc/mecha/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
+    device/htc/mecha/dsp/AIC3254_REG_DualMicXD01.csv:system/etc/AIC3254_REG_DualMicXD01.csv \
+    device/htc/mecha/dsp/AIC3254_REG_DualMicXD02.csv:system/etc/AIC3254_REG_DualMicXD02.csv
+
+# firmware
+PRODUCT_COPY_FILES += \
+    device/htc/mecha/firmware/default.acdb:system/etc/firmware/default.acdb \
+    device/htc/mecha/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
+    device/htc/mecha/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
+    device/htc/mecha/firmware/default_mfg.acdb:system/etc/firmware/default_mfg.acdb
+
+# keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/mecha/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/htc/mecha/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/htc/mecha/prebuilt/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/mecha/prebuilt/usr/keylayout/mecha-keypad.kl:system/usr/keylayout/mecha-keypad.kl \
+    device/htc/mecha/prebuilt/usr/keylayout/mecha-keypad-v0.kl:system/usr/keylayout/mecha-keypad-v0.kl \
+    device/htc/mecha/prebuilt/usr/keylayout/atmel-touchscreen.kl:system/usr/keylayout/atmel-touchscreen.kl
+
+# permissions
+PRODUCT_COPY_FILES += \
+    device/htc/mecha/prebuilt/etc/permissions/vzwapnlib.xml:system/etc/permissions/vzwapnlib.xml \
+    device/htc/mecha/prebuilt/etc/permissions/com.verizon.phone.xml:system/etc/permissions/com.verizon.phone.xml \
+    device/htc/mecha/prebuilt/etc/permissions/com.verizon.hardware.telephony.lte.xml:system/etc/permissions/com.verizon.hardware.telephony.lte.xml \
+    device/htc/mecha/prebuilt/etc/permissions/com.verizon.hardware.telephony.ehrpd.xml:system/etc/permissions/com.verizon.hardware.telephony.ehrpd.xml
+
+# touchscreen
+PRODUCT_COPY_FILES += \
+    device/htc/mecha/prebuilt/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+    device/htc/mecha/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
+
+# products
+PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_LOCALES += en
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# packages
 PRODUCT_PACKAGES += \
     gps.mecha \
     libbt-vendor
 
-# amtel
-PRODUCT_COPY_FILES += \
-    device/htc/mecha/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc
-
-# keychars and keylayouts
-PRODUCT_COPY_FILES += \
-    device/htc/mecha/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
-    device/htc/mecha/usr/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-    device/htc/mecha/usr/keychars/mecha-keypad.kcm.bin:system/usr/keychars/mecha-keypad.kcm.bin \
-    device/htc/mecha/usr/keychars/mecha-keypad-v0.kcm.bin:system/usr/keychars/mecha-keypad-v0.kcm.bin \
-    device/htc/mecha/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/htc/mecha/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-    device/htc/mecha/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/htc/mecha/usr/keylayout/mecha-keypad-v0.kl:system/usr/keylayout/mecha-keypad-v0.kl \
-    device/htc/mecha/usr/keylayout/mecha-keypad.kl:system/usr/keylayout/mecha-keypad.kl
-
-# firmware
-PRODUCT_COPY_FILES += \
-    device/htc/mecha/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd \
-    device/htc/mecha/firmware/default.acdb:system/etc/firmware/default.acdb \
-    device/htc/mecha/firmware/default_mfg.acdb:system/etc/firmware/default_mfg.acdb \
-    device/htc/mecha/firmware/default_org.acdb:system/etc/firmware/default_org.acdb
-
-# dsp
-PRODUCT_COPY_FILES += \
-    device/htc/mecha/dsp/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
-    device/htc/mecha/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
-    device/htc/mecha/dsp/AIC3254_REG_DualMicXD01.csv:system/etc/AIC3254_REG_DualMicXD01.csv \
-    device/htc/mecha/dsp/AIC3254_REG_DualMicXD02.csv:system/etc/AIC3254_REG_DualMicXD02.csv \
-    device/htc/mecha/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
-    device/htc/mecha/dsp/AudioBTID.csv:system/etc/AudioBTID.csv \
-    device/htc/mecha/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
-    device/htc/mecha/dsp/CodecDSPID_BCLK.txt:system/etc/CodecDSPID_BCLK.txt \
-    device/htc/mecha/dsp/HP_Audio.csv:system/etc/HP_Audio.csv \
-    device/htc/mecha/dsp/HP_Video.csv:system/etc/HP_Video.csv \
-    device/htc/mecha/dsp/SPK_Combination.csv:system/etc/SPK_Combination.csv \
-    device/htc/mecha/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv
+# overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.oem.ecclist=112,911 \
+    ro.ril.enable.a52=0 \
+    ro.ril.enable.a53=1 \
+    ro.ril.def.agps.mode=6 \
+    ro.com.google.clientidbase=android-htc \
+    ro.com.google.clientidbase.gmm=android-htc \
+    ro.com.google.clientidbase.yt=android-verizon \
+    ro.com.google.clientidbase.am=android-verizon \
+    ro.com.google.clientidbase.ms=android-verizon \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.setupwizard.enable_bypass=1 \
+    ro.telephony.call_ring.delay=500 \
+    ro.telephony.call_ring.multiple=false \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
 
 # profiles
 PRODUCT_COPY_FILES += $(shell \
@@ -108,15 +122,4 @@ PRODUCT_COPY_FILES += $(shell \
     | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/etc\/soundimage\/\2/' \
     | tr '\n' ' ')
 
-# Default sounds and ringtones
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Hydra.ogg \
-    ro.config.notification_sound=Proxima.ogg \
-    ro.config.alarm_alert=Plutonium.ogg
-
-# Enable root access for debugging
-ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=3 \
-    ro.debuggable=1 \
-    ro.secure=0
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
